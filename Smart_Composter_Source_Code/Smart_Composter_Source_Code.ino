9a098df2074118d7e5eec3c3c05d16b7933d593d
meeloo1323 <sungjoon101@hotmail.com>
2023-01-10 11:13:52 -0800

    Cleaned up source code
    Rearranged the beginning for organization purposes.
    
    Removed sensors that we aren't using.

--- Smart_Composter_Source_Code/Smart_Composter_Source_Code.ino
+++ Smart_Composter_Source_Code/Smart_Composter_Source_Code.ino
@@ -1,16 +1,11 @@
 /*
- * Smart Composter code
- * Written by: Sung-Joon Im, Carlos Olmos, previous Compostify Team
- */
+Smart Composter code
+Written by: Sung-Joon Im, Carlos Olmos, previous Compostify Team
+*/
 
 //==================== Libraries ====================
-#include <HTU21D.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-//==================== Pins ====================
-//OneWire bus pin
-#define ONE_WIRE_BUS 2
 
 //==================== Constants ====================
 //Humidity percentage low (When is it too dry)
@@ -25,57 +20,52 @@ const int temperatureLow = 13;
 //Temperature high (When is it too hot, Celsius)
 const int temperatureHigh = 68;
 
-//==================== Sensors and stuff ====================
-//*HTU@1D_F: temperature and humidity sensor*
-HTU21D sensor;
+//==================== Pins ====================
+//OneWire bus pin (This will be our temperature sensor)
+const int ONE_WIRE_BUS 2;
 
-//*DS18B20: temperature sensor*
+//Moisture sensor pin (analog in)
+const int MOISTURE_SENSOR 14;
+
+//Manual tumble push button detection pin (digital in)
+const int TUMBLE_BUTTON 13;
+
+//Float switch detection pin (digital in)
+const int FLOAT_SWITCH 7;
+
+//Motor transistor (output)
+const int MOTOR_TRANSISTOR 6;
+
+//Water pump transistor (output)
+const int WATER_PUMP_TRANSISTOR 5;
+
+//LEDs to show the user the state of the system
+const int WET_LED 12; //digital output
+const int DRY_LED 11; //digital output
+const int COLD_LED 10; //digital output
+const int HOT_LED 9; //digital output
+
+//==================== Sensors and stuff ====================
+//DS18B20: temperature sensor
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // ==================== Setup and Loop ==================== This whole thing will have to be double checked and revised later on
 void setup() 
-{
-  
-  //*HTU@1D_F: temperature and humidity sensor*
-  sensor.begin();
-  
+{ 
   Serial.begin(9600); //set the serial port to 9600
   delay(500);
 }
 
+/*
+Read in sensor data
+Figure out if the tumbler is too hot, cold, wet, or dry
+Activate water pump if too dry
+Activate tumbler when hot, cold, wet, or dry
+*/
 void loop() 
-{
-  //*HTU@1D_F: temperature and humidity sensor*
-  float temperature = sensor.getTemperature();
-  float humidity = sensor.getHumidity();
-  
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println("C");
-  
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println("%");
-  
-  //*MQ: gas detect sensor*
-  int mq = analogRead(A0);
-  Serial.print("Gas sensor value: ");
-  Serial.print(mq);
-  //over 300 consider as smoke
-  if(mq>300)
-  {
-    Serial.print(" > Smoke detected!");
-  }
-  Serial.println("");
-  
-  if(mq<300)
-  {
-    Serial.print(" > this gas level is fine.");
-  }
-  Serial.println("");
-  
+{ 
   //*DS18B20: temperature sesnor*
   sensors.requestTemperatures(); 
   Serial.print("Temperature: ");
@@ -83,31 +73,15 @@ void loop()
   Serial.println(" F");
   
   //*soil moisure sensor V2.0*
-  int soil = analogRead(A0); 
+  int soil = analogRead(MOISTURE_SENSOR); 
   //translate to scale 0 - 100%
   int percentageHumidity = map(soil, wet, dry, 100, 0);
   Serial.print("Humidity: ");
   Serial.print(percentageHumidity);
   Serial.println("%");
-  
-  //*HC-SR04: ultrasonic sensor*
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  
-  digitalWrite(trigPin, LOW);
-  
-  duration = pulseIn(echoPin, HIGH);
-  
-  distance = duration*0.034/2;
-  
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
 
-    
+
+
   Serial.println();
   delay(1000);
 }
